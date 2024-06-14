37f2dc3a45696d5a5cf59ee98b4262b6f8f76b9e
yomna <yumnamohamed30@gmail.com>
2024-06-14 18:08:10 +0300

    editting database connection

--- Assignment 4.2/database/coonection.js
+++ Assignment 4.2/database/coonection.js
@@ -2,16 +2,12 @@ import  sql from 'mysql2';
 
 export const dbConnection = ()=>{
 
-    // const connection = sql.createConnection({
-    //     host: 'localhost',
-    //     user: 'root',
-    //     password: '',
-    //     database: 'linked_posts'
-    //     // database: 'node_db'
-    // })
-// = retuen sql.createconnection ({ })
-
-     const connection = sql.createConnection('mysql://uiqc8si81wjrbpl8:BATU5nTS4JaSXJ24yF7J@bj5c8eambm4q2kiswdsq-mysql.services.clever-cloud.com:3306/bj5c8eambm4q2kiswdsq')
+    const connection = sql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'linked_posts'
+    })
 
     connection.connect((err)=>{ // first error parameter
         if(err) return console.log('database error ...', err);
